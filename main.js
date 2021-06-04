6837ebd9af04995a97e402a9893f5ff7fdec58ec
Samantha Solomon <sleahsolomon@gmail.com>
2021-06-04 16:32:08 -0400

    add main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+// Meetup API GET Request
+
+const meetUpURL = "https://api.meetup.com/charmcityjs/events?scroll=next_upcoming&page=20&has_ended=false&omit=created,duration,rsvp_limit,date_in_series_pattern,time,updated,utc_offset,waitlist_count"
