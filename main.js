9a5fb5e49ecd2a5681f7012e6a81fd0fbe4809c5
Samantha Solomon <sleahsolomon@gmail.com>
2021-06-04 18:15:02 -0400

    AJAX request and render with dummy data

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-// Meetup API GET Request
+// MEETUP API GET REQUEST
 
-const meetUpURL = "https://api.meetup.com/charmcityjs/events?scroll=next_upcoming&page=20&has_ended=false&omit=created,duration,rsvp_limit,date_in_series_pattern,time,updated,utc_offset,waitlist_count"
+// VARIABLES
+// const meetUpURL = "https://api.meetup.com/charmcityjs/events?scroll=next_upcoming&page=20&has_ended=false&omit=created,duration,rsvp_limit,date_in_series_pattern,time,updated,utc_offset,waitlist_count"
+const meetUpDiv = document.getElementById('upcoming-meetups')
+
+// FETCH DATA
+let req = new XMLHttpRequest();
+
+req.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+
+req.onload = () => {
+    const data = JSON.parse(req.responseText)
+    // console.log('data-', data)
+    render(data)
+}
+req.send()
+
+// RENDER DATA FUNCTION
+let render = (event) => {
+    let meetUpHTML = ""
+    for (i = 0; i < event.length; i++){
+        meetUpHTML += "<p>" + event[i].name + " is a " + event[i].species + ".</p>"
+    }
+    meetUpDiv.insertAdjacentHTML('beforeend', meetUpHTML)
+
+}
