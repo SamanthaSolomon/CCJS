5b990cd119b48580626843f95dfbc76d77cf189a
Samantha Solomon <sleahsolomon@gmail.com>
2021-07-16 15:27:03 -0400

    typing effect on h1 complete

--- JS/app.js
+++ JS/app.js
@@ -2,29 +2,46 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM loaded')
 
-    const textDisplay = document.getElementsByClassName('typing')
+    const h1Display = document.getElementById('typing')
+
     const h1 = ['< charmCityJS />', '< charmCityJS />', '< charmCityJS />']
     let i = 0
     let x = 0
     let currentH1 = []
+    let isDeleting = false
 
 
     function loop(){
-        textDisplay.innerHTML = currentH1
+        h1Display.innerHTML = currentH1.join('')
         if (i < h1.length) {
-            console.log(h1[i])
 
-            if (x < h1[i].length) {
-                console.log(h1[i][x])
+            if (!isDeleting && x <= h1[i].length) {
                 currentH1.push(h1[i][x])
                 x++
+                console.log('add letter', x)
             }
 
-            if (x == h1[i].length) {
-                i++
+            if (isDeleting && x <= h1[i].length){
+                currentH1.pop(h1[i][x])
+                x--
+                console.log('delete letter', x)
             }
+
+            if (x === h1[i].length) {
+                isDeleting = true
+            }
+
+            if (isDeleting && x === 0){
+                currentPhrase = []
+                isDeleting = false
+                i++
+                if (i === h1.length){
+                    i = 0
+                }
+            }
+
         }
-        setTimeout(loop, 500)
+        setTimeout(loop, 400)
     }
     loop()
 })
